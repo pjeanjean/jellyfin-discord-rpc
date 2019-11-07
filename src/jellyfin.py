53c598fa494559033991ed906e1db0bce945c175
RhiobeT <rhiobet@gmail.com>
2019-11-07 16:49:28 +0100

    Initial prototype

---
+++ src/jellyfin.py
@@ -0,0 +1,29 @@
+from pypresence import Presence
+from settings import DISCORD_CLIENT_ID, JELLYFIN_SERVER_URL, JELLYFIN_API_KEY, JELLYFIN_USER_ID
+import requests
+import shutil
+import time
+
+
+def update():
+    r_sessions = requests.get(f'{JELLYFIN_SERVER_URL}/Sessions?ControllableByUserId={JELLYFIN_USER_ID}&api_key={JELLYFIN_API_KEY}')
+    sessions = r_sessions.json()
+    if sessions:
+        if 'NowPlayingItem' in sessions[0]:
+            playing = sessions[0]['NowPlayingItem']
+            if playing['Type'] == 'Audio':
+                t = (int) (sessions[0]['PlayState']['PositionTicks'] / 10000000)
+                RPC.update(details=playing['Name'], state=playing['Artists'][0], large_image='audio',
+                           start=((int) (time.time() - t)))
+            else:
+                print(f'{playing["Type"]} support not yet implemented')
+    else:
+        print('No session found :shrug:')
+
+
+RPC = Presence(DISCORD_CLIENT_ID)
+RPC.connect()
+
+while True:
+    update()
+    time.sleep(15)
